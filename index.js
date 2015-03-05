767d82477b63b8b6167c79e6328a87938f20cd77
ajsheffield <andrew.sheffield@sv-code.com>
2015-03-05 15:43:01 -0800

    Working on getting complete loop through database

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 var express = require('express');
 var app = express();
-var backbone = require('backbone');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 
-var Meal = Backbone.Model.extend({
-  defaults: {
-    "appetizer":  "caesar salad",
-    "entree":     "ravioli",
-    "dessert":    "cheesecake"
-  }
-});
-
 app.get('/', function(request, response) {
-  response.send("Dessert will be " + (new Meal).get('dessert'));
+  response.sendFile(__dirname + '/public/test.html');
 });
 
 app.listen(app.get('port'), function() {
