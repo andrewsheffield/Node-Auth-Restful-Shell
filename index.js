800bd81d960dbc949ff8e0a05efb2df8c78da836
ajsheffield <andrew.sheffield@sv-code.com>
2015-03-17 13:47:42 -0700

    Added body-parser to project dependency and got post form action type to work through a new page

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/public/index.html');
+  	response.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/dashboard/', function(request, response) {
+	var data = {
+        email : request.body.email,
+        password : request.body.password
+    };
+	response.send(data);
+  	//response.sendFile(__dirname + '/public/dashboard.html');
 });
 
 app.listen(app.get('port'), function() {
