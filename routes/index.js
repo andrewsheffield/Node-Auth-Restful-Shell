7411b4d69d2a186c02402bef44db5f87c95a4252
ajsheffield <andrew.sheffield@sv-code.com>
2015-04-06 17:59:04 -0700

    Found the error in sending the cookie after the page was being sent. Tried to reorder some of the processes.

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ function ensureAuth(req, res, next) {
 	if (req.isAuthenticated()) {
 		next();
 	} else {
-		res.reroute('/');
+		res.redirect('/');
 	}
 }
 
@@ -40,12 +40,13 @@ router.post('/', passport.authenticate('local',
 	function (req, res, next) {
 		if (!req.body.rememberme) { return next(); }
 
-		req.user.rmToken = crypto.randomBytes(16).toString('hex');
-        bcrypt.hash(req.user.rmToken, 10, function(err, hash) {
-          res.cookie('remember_me', req.user.username + " " + hash, { maxAge: (30 * 24 * 60 * 60 * 1000), httpOnly: true });
-        });
+		
         req.user.save(function(err, user) {
-          res.redirect('/');
+        	user.rmToken = crypto.randomBytes(16).toString('hex');
+        	bcrypt.hash(user.rmToken, 10, function(err, hash) {
+          		res.cookie('remember_me', user.username + " " + hash, { maxAge: (30 * 24 * 60 * 60 * 1000), httpOnly: true });
+          		return next();
+        	});
 		});
 	},
 	function (req, res) {
